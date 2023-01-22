e9445dcce994ddc8b2969f3615eed81652429ac8
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2023-01-22 13:35:11 -0300

    feat: add CORS policy

--- src/main/java/com/springbootrest/SpringbootRestApplication.java
+++ src/main/java/com/springbootrest/SpringbootRestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootRestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootRestApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootRestApplication.class, args);
+  }
 
 }
--- src/main/java/com/springbootrest/controllers/PersonController.java
+++ src/main/java/com/springbootrest/controllers/PersonController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,6 +48,7 @@ public class PersonController {
     return service.findAll();
   }
 
+  @CrossOrigin(origins = "http://localhost:8080")
   @GetMapping(value = "/{id}", produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
   @Operation(summary = "Finds a Person", description = "Finds a Person",
       tags = {"People"},
@@ -65,6 +67,7 @@ public class PersonController {
     return service.findById(id);
   }
 
+  @CrossOrigin(origins = "http://localhost:8080")
   @PostMapping(consumes = org.springframework.http.MediaType.APPLICATION_JSON_VALUE, produces = org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
   @Operation(summary = "Adds a new Person",
       description = "Adds a new Person by passing in a JSON, XML or YML representation of the person!",
